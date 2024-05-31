22aa08e4685d2c2e512836be196f01a915e9b612
Oksana <benefit989@gmail.com>
2024-05-31 10:18:49 +0300

    add prev and next day in header

--- src/scripts/calendar/header.js
+++ src/scripts/calendar/header.js
@@ -6,31 +6,40 @@ const daysOfWeek = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 const calendarHeaderElement = document.querySelector('.calendar__header');
 const createNewEventsButtonElement = document.querySelector('.create-event-btn');
 
+const createNameOfDayElement = (nameOfDay, dayOfWeek) => {
+  const today = new Date(new Date().setHours(0,0,0,0));
+  let result = '';
+
+  if (dayOfWeek > today) {
+    result = `
+      <div class="calendar__day-label day-label">
+        <span class="day-label__day-name">${nameOfDay}</span>
+        <span class="day-label__day-number_next-color">${dayOfWeek.getDate()}</span>
+      </div>`
+  } else if (dayOfWeek < today) {
+    result = `
+      <div class="calendar__day-label day-label">
+        <span class="day-label__day-name">${nameOfDay}</span>
+        <span class="day-label__day-number_prew-color">${dayOfWeek.getDate()}</span>
+      </div>`;
+  } else {
+    result = `
+      <div class="calendar__day-label day-label today">
+        <span class="day-label__day-name today__day-name">${nameOfDay}</span>
+        <span class="day-label__day-number today__day-number">${dayOfWeek.getDate()}</span>
+      </div>`;
+  }
+
+  return result;
+}
+
 export const renderHeader = () => {
   const numberOfDaysOfWeek = generateWeekRange(getItem('displayedWeekStart'));
-  let dateOfWeek = '';
-  const todayDateSrting = new Intl.DateTimeFormat('en-US').format(new Date());
 
   calendarHeaderElement.innerHTML = [
     `<div class="calendar__time-zon"><span>${new Date().toString().match(/([A-Z]+[\+-][0-9][0-9])/)[1]}</span></div>`, 
-    ...daysOfWeek.map((day, index) => {
-      dateOfWeek = numberOfDaysOfWeek[index];
-      
-      if(new Intl.DateTimeFormat('en-US').format(dateOfWeek) === todayDateSrting) {
-        return `
-          <div class="calendar__day-label day-label today">
-            <span class="day-label__day-name today__day-name">${day.toUpperCase()}</span>
-            <span class="day-label__day-number today__day-number">${dateOfWeek.getDate()}</span>
-          </div>`
-      }
-
-      return `
-        <div class="calendar__day-label day-label">
-          <span class="day-label__day-name">${day.toUpperCase()}</span>
-          <span class="day-label__day-number">${dateOfWeek.getDate()}</span>
-        </div>`
-    }
-  )].join('');
+    ...daysOfWeek.map((nameOfDay, index) => createNameOfDayElement(nameOfDay.toUpperCase(), numberOfDaysOfWeek[index]))]
+    .join('');
 };
 
 // при клике на кнопку "Create" открыть модальное окно с формой для создания события
