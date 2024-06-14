b0980488f77dae71a3b70a81665735c22a3fd7fe
Oksana <benefit989@gmail.com>
2024-06-14 16:32:38 +0300

    fix first render

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,13 +6,22 @@ import { setItem } from './common/storage.js';
 import { getStartOfWeek } from './common/time.utils.js';
 import { initEventForm } from './events/createEvent.js';
 import { updateEvents } from './events/changeEvent.js';
+import { getEventsLists } from './common/getEway.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   renderTimescale();
   setItem('displayedWeekStart', getStartOfWeek(new Date()));
-  renderWeek();
-  renderHeader();
-  initNavigation();
-  initEventForm();
-  updateEvents();
+  try {
+    getEventsLists()
+      .then(list => {
+        setItem('events', list);
+        renderWeek();
+        renderHeader();
+        initNavigation();
+        initEventForm();
+        updateEvents();
+      });
+  } catch(err) {
+    alert(err.message);
+  };
 });
